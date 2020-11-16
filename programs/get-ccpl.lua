63f66409d218920b33beb414f34dd087dca5331b
Brady <bradyakent@gmail.com>
2020-11-16 15:36:43 -0600

    Update get-ccpl.lua

--- programs/get-ccpl.lua
+++ programs/get-ccpl.lua
@@ -1,5 +1,9 @@
 local logFile = fs.open("/log.txt","w")
 
+local function outputLog(input)
+    logFile.writeLine(input)
+    print(input)
+end
 
 
 local args = { ... }
@@ -26,9 +30,9 @@ local function parseURL(URL)
 	local z, last = URL:find("github.com/",1,true)
     local snip = URL:sub(last+1,URL:len()+1)
     local URLpath = {}
-    logFile.writeLine("Snipping URL:")
+    outputLog("Snipping URL:")
     for x in snip:gmatch("%w+") do
-        logFile.writeLine(x)
+        outputLog(x)
 		URLpath[#URLpath+1] = x
     end
     if not URLpath[4] then
@@ -36,14 +40,14 @@ local function parseURL(URL)
     end
     local apiResult = http.get("https://api.github.com/repos/"..URLpath[1].."/"..URLpath[2].."/branches/"..URLpath[4])
     local apiObj = textutils.unserializeJSON(apiResult.readAll())
-    logFile.writeLine("Grabbing treeObj from")
-    logFile.writeLine(apiObj.commit.commit.tree.url.."?recursive=1")
+    outputLog("\nGrabbing treeObj from")
+    outputLog(apiObj.commit.commit.tree.url.."?recursive=1")
     local treeObj = textutils.unserializeJSON(http.get(apiObj.commit.commit.tree.url.."?recursive=1").readAll())
 
-    logFile.writeLine("Building simpleTreeObj:")
+    outputLog("\nBuilding simpleTreeObj:")
     local simpleTreeObj = {}
     for i=1,#treeObj.tree do
-        logFile.writeLine(treeObj.tree[i].path)
+        outputLog(treeObj.tree[i].path)
         simpleTreeObj[#simpleTreeObj+1] = {path=treeObj.tree[i].path, type=treeObj.tree[i].type}
     end
     local result = {
@@ -60,18 +64,18 @@ if args[1] then
     sourceURL = args[1]
 end
 
-logFile.writeLine("Parsing URL...")
+outputLog("Parsing URL...")
 local info = parseURL(sourceURL)
-logFile.writeLine("URL parsed!")
+outputLog("URL parsed!")
 
-logFile.writeLine("Creating file structure/downloading files:")
+outputLog("\nCreating file structure/downloading files:")
 for i, item in ipairs(info.treeObj) do
     if not includes(ignore, item.path) then
         if item.type == "tree" then
-            logFile.writeLine("Dir found! Creating /CCPL/"..item.path)
+            outputLog("Dir found! Creating /CCPL/"..item.path)
             fs.makeDir("/CCPL/"..item.path)
         elseif item.type == "blob" then
-            logFile.writeLine("File found! Downloading /CCPL/"..item.path)
+            outputLog("File found! Downloading /CCPL/"..item.path)
             local dataToWrite = http.get("https://raw.githubusercontent.com/"..info.owner.."/"..info.repo.."/"..info.tree.."/"..item.path).readAll()
             local fileToWrite = fs.open("/CCPL/"..item.path,"w")
             fileToWrite.write(dataToWrite)
@@ -79,4 +83,4 @@ for i, item in ipairs(info.treeObj) do
         end
     end
 end
-logFile.writeLine("Finished!")
+outputLog("\nFinished!")
