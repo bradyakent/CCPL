8f9af58fd1ba58430817489668c4946fa4c001b5
Brady <bradyakent@gmail.com>
2020-11-16 19:37:22 -0600

    Update completions.lua

--- startup/completions.lua
+++ startup/completions.lua
@@ -1,7 +1,13 @@
 local complete = require("cc.shell.completion")
 local CCPLdir = fs.getDir(fs.getDir(shell.getRunningProgram()))
 
+local function fileIf(theShell, thisArg, prevArgs)
+    if prevArgs[1] == "update" then
+        return complete.file(theShell, thisArg)
+    end
+end
+
 shell.setCompletionFunction(CCPLdir.."/programs/gist.lua",complete.build(
     { complete.choice, {"install ", "update "} },
-    complete.file
+    fileIf
 ))
