41ee566586a3bcc9b7a62415a53fc5c886527086
Brady <bradyakent@gmail.com>
2020-11-16 20:01:47 -0600

    Update completions.lua

--- startup/completions.lua
+++ startup/completions.lua
@@ -2,8 +2,7 @@ local complete = require("cc.shell.completion")
 local CCPLdir = fs.getDir(fs.getDir(shell.getRunningProgram()))
 
 local function fileIf(theShell, thisArg, prevArgs)
-    print(textutils.serialize(prevArgs))
-    if prevArgs[1] == "update" then
+    if prevArgs[2] == "update" then
         return complete.file(theShell, thisArg)
     end
 end
