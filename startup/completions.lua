ac7d8f92a377331d27054145241dd02a7e266bf7
Brady <bradyakent@gmail.com>
2020-11-16 20:23:37 -0600

    make completion for farm.lua

--- startup/completions.lua
+++ startup/completions.lua
@@ -1,13 +1,17 @@
 local complete = require("cc.shell.completion")
 local CCPLdir = fs.getDir(fs.getDir(shell.getRunningProgram()))
 
-local function fileIf(theShell, thisArg, prevArgs)
-    if prevArgs[2] == "update" then
+local function fileIf(theShell, thisArg, prevArgs, text)
+    if prevArgs[2] == text then
         return complete.file(theShell, thisArg)
     end
 end
 
+-- programs/gist.lua
 shell.setCompletionFunction(CCPLdir.."/programs/gist.lua",complete.build(
     { complete.choice, {"install ", "update "} },
-    fileIf
+    { fileIf, "update " }
 ))
+
+--programs/farm.lua
+shell.setCompletionFunction(CCPLdir.."/programs/farm.lua",complete.build({ complete.choice, {"create ", "harvest "} }))
