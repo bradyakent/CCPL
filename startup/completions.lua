de59a3d668460b35b40d4e61e63c6077e8344216
Brady <bradyakent@gmail.com>
2020-11-18 09:52:16 -0600

    Fix completions.lua path to programs

--- startup/completions.lua
+++ startup/completions.lua
@@ -8,10 +8,10 @@ local function fileIf(theShell, thisArg, prevArgs, text)
 end
 
 -- programs/gist.lua
-shell.setCompletionFunction(CCPLPath.."/programs/gist.lua",complete.build(
+shell.setCompletionFunction(CCPLPath.."ccpl/programs/gist.lua",complete.build(
     { complete.choice, {"install ", "update "} },
     { fileIf, "update " }
 ))
 
 --programs/farm.lua
-shell.setCompletionFunction(CCPLPath.."/programs/farm.lua",complete.build({ complete.choice, {"create ", "harvest "} }))
+shell.setCompletionFunction(CCPLPath.."ccpl/programs/farm.lua",complete.build({ complete.choice, {"create ", "harvest "} }))
