7535ba2befee013bd475daf14665dc1b8b65aa4d
Brady <bradyakent@gmail.com>
2020-11-17 19:40:30 -0600

    Fix require formatting

--- programs/farm.lua
+++ programs/farm.lua
@@ -1,6 +1,6 @@
 -- implements basic farming API usage
 local _p = settings.get("ccpl.path")
-local farming = require(_p:sub(2).."ccpl.farming")
+local farming = require(_p.."ccpl.apis.farming")
 
 local function printUsage()
     print("Usage:")
