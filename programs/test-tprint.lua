0d80376d11d10e2355adf8ef67c75a7181976965
Brady <bradyakent@gmail.com>
2020-11-18 21:39:47 -0600

    Update test-tprint.lua
    lets see if tprint.lua works now

--- programs/test-tprint.lua
+++ programs/test-tprint.lua
@@ -3,10 +3,11 @@ local tprint = require(_p.."ccpl.apis.tprint")
 
 if read() == "yes" then
     local output = fs.open("test.hob", "w")
-    output.write(textutils.serialize(tprint.scan(3, 3, 3)))
+    output.write(textutils.serialize(tprint.scan("Test", 3, 3, 3)))
     output.close()
 else
     local input = fs.open("test.hob", "r")
     tprint.print(textutils.unserialize(input.readAll()))
     input.close()
 end
+
