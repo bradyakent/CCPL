e855629633a63d44928e7e32d2d31b8059f378e2
Brady <bradyakent@gmail.com>
2020-12-02 16:50:08 -0600

    fix completions
    Completions are an absolute path without the leading slash.

--- startup/completions.lua
+++ startup/completions.lua
@@ -7,13 +7,13 @@ local function fileIf(theShell, thisArg, prevArgs, text)
 end
 
 -- programs/gist.lua
-shell.setCompletionFunction("/ccpl/programs/gist.lua",complete.build(
+shell.setCompletionFunction("ccpl/programs/gist.lua",complete.build(
     { complete.choice, {"install ", "update "} },
     { fileIf, "update " }
 ))
 
 --programs/farm.lua
-shell.setCompletionFunction("/ccpl/programs/farm.lua",complete.build({ complete.choice, {"create ", "harvest "} }))
+shell.setCompletionFunction("ccpl/programs/farm.lua",complete.build({ complete.choice, {"create ", "harvest "} }))
 
 --programs/3dprint.lua
 local function printCompletion(theShell, argument, prevArgs)
@@ -23,7 +23,7 @@ local function printCompletion(theShell, argument, prevArgs)
         return complete.file(theShell, argument)
     end
 end
-shell.setCompletionFunction("/ccpl/programs/3dprint.lua",complete.build({ printCompletion, many=true }))
+shell.setCompletionFunction("ccpl/programs/3dprint.lua",complete.build({ printCompletion, many=true }))
 
 -- programs/warehouse.lua
-shell.setCompletionFunction("/ccpl/programs/warehouse.lua",complete.build({ complete.choice, {"new ", "list", "get ", "put "} }))
+shell.setCompletionFunction("ccpl/programs/warehouse.lua",complete.build({ complete.choice, {"new ", "list", "get ", "put "} }))
