9cc03e6b87c582a30d942911adf889cd588a5e74
Brady <bradyakent@gmail.com>
2020-12-03 07:08:29 -0600

    Update wh-gui.lua
    Help pages updated, Search fixed, listing display after search now scrolls and renders correctly, remove usage of read()

--- programs/wh-gui.lua
+++ programs/wh-gui.lua
@@ -18,31 +18,55 @@ function closeButton:onClick()
     mainLoop = false
 end
 
-local search = {}
 local searchBar = gui.Object:new(screen, 1, screen.height, screen.width, 1)
-function searchBar:onClick()
-    searchBar:erase()
-    screen:render()
-    term.setCursorPos(self.x,self.y)
-    local userInput = read()
-    local keys = {}
-    for key in userInput:gmatch("@?[%w_]+") do
-		keys[#keys+1] = key
-	end
-	for key in userInput:gmatch("@?[%w[^_]]+") do
-		keys[#keys+1] = key
-	end
-    search.keys = keys
-end
-function search:match(itemName)
-    for _, key in ipairs(self.keys) do
-		if key:sub(1,1) == "@" then
-			if itemName:find(key:sub(2), 1, true) then return true end
-        else
-			if itemName:find(key, itemName:find(":"), true) then return true end
-		end
+local search = { keys={} }
+function search:filter(list)
+    if not self.keys[1] then return list end
+    local returnList = {}
+    for _, item in ipairs(list) do
+        local attachedKeys = {}
+        for _, key in ipairs(self.keys) do
+            attachedKeys[#attachedKeys+1] = key
+            if not key.attach then
+                local passed = true
+                for _, part in ipairs(attachedKeys) do
+                    if part.amount and item.amount < part.amount then
+                        passed = false
+                        break
+                    end
+                    if part.modName then
+                        if not item.name:find(part.text, 1, true) then
+                            passed = false
+                            break
+                        end
+                    else
+                        if not item.name:find(part.text, item.name:find(":")+1, true) then
+                            passed = false
+                            break
+                        end
+                    end
+                end
+                if passed then
+                    returnList[#returnList+1] = item
+                    break
+                end
+                attachedKeys = {}
+            end
+        end
     end
-    return false
+    return returnList
+end
+function search:generateKeys(userInput)
+    local keys = {}
+    for modName, text, attach, amount in userInput:gmatch("(@?)([%w_]+)(&?)(#?%d*)") do
+        keys[#keys+1] = {
+            text=text,
+            modName=(modName == "@"),
+            amount=(amount:sub(1,1) == "#" and tonumber(amount:sub(2)) or -1),
+            attach=(attach == "&")
+        }
+    end
+    self.keys = keys
 end
 
 local listingLabel = gui.Object:new(screen, 1, 1, screen.width-6, 1)
@@ -73,7 +97,7 @@ local function handleInput(object)
     listing.requested[object.linkedLocation] = tonumber(userInput) or 0
     if (object.linkedLocation > listing.requested.n) then listing.requested.n = object.linkedLocation end
     searchBar:erase()
-    init()
+    searchBar:write(1, 1, "Click here to search...", colors.gray)
 end
 
 for line=1,listing.height do
@@ -86,31 +110,32 @@ for line=1,listing.height do
 end
 
 function listing:populate()
-    local skips = 0
     for index, displayObject in ipairs(listing.objects) do
         displayObject:erase()
-        local item
-        local printed = false
-        while not printed do
-            item = listing.list[skips + index + listing.scrollOffset]
-            if not item then return end
-            if not search.keys or search:match(item.name) then
-                local itemText = item.location..": "..item.name:sub(item.name:find(":")+1)
-                if #itemText > listing.width - 10 then
-                    itemText = itemText:sub(1,listing.width - 13).."..."
-                end
-                displayObject.helpText = item.name:sub(item.name:find(":")+1)
-                displayObject:write(1, 1, itemText)
-                displayObject:write(displayObject.width-#tostring(item.amount), 1, tostring(item.amount))
-
-                local input = listing.inputs[index]
-                input.linkedLocation = item.location
-                input:erase()
-                input:write(5-#tostring(listing.requested[item.location] or 0), 1, tostring(listing.requested[item.location] or 0))
-                printed = true
-            else
-                skips = skips + 1
+        displayObject.helpText = nil -- remove help text
+        listing.inputs[index].linkedLocation = nil -- remove linked location for input
+        local item = listing.list[index + listing.scrollOffset]
+        if item then
+            local itemText = item.location..": "..item.name:sub(item.name:find(":")+1)
+            if #itemText > listing.width - 10 then
+                itemText = itemText:sub(1,listing.width - 13).."..."
             end
+            displayObject.helpText = item.name:sub(item.name:find(":")+1)
+            displayObject:write(1, 1, itemText)
+            displayObject:write(displayObject.width-#tostring(item.amount), 1, tostring(item.amount))
+
+            local input = listing.inputs[index]
+            input:fill(colors.gray)
+            input.linkedLocation = item.location
+            local stringToDisplay = "0"
+            if listing.requested[item.location] and listing.requested[item.location] > 0 then
+                stringToDisplay = tostring(listing.requested[item.location])
+            end
+            input:erase()
+            input:write(5-stringToDisplay:len(), 1, stringToDisplay)
+        else
+            listing.inputs[index]:fill(colors.black)
+            listing.inputs[index]:erase()
         end
     end
 end
@@ -202,6 +227,7 @@ local function init()
     listingLabel:write(1, 1, "i   Block", colors.white)
     listingLabel:write(listingLabel.width-10, 1, "Stock Pull", colors.white)
 
+    listing:erase()
     listing:draw(listing.width-4, 1, colors.gray, 4, listing.height)
 
     putButton:fill(colors.blue)
@@ -218,30 +244,111 @@ local function init()
     clearButton:write(2, 2, "Pulls", colors.black)
 end
 
+function searchBar:onClick(_, _, prepend)
+    local userInput = prepend or ""
+    search.keys = {}
+    listing.list = search:filter(storage.list())
+    listing.scrollOffset = 0
+    local cursorX, cursorY = userInput:len()+1, 1
+
+    listing:populate()
+    searchBar:erase()
+    searchBar:write(1,1,userInput,colors.white)
+    screen:render()
+    term.setCursorPos(cursorX + self.x - 1, cursorY + self.y - 1)
+    term.setCursorBlink(true)
+
+    local inputLoop = true
+    local event = {}
+    while inputLoop do
+        -- Handle events
+        event = { os.pullEvent() }
+        if event[1] == "char" then
+            searchBar:write(cursorX,cursorY,event[2],colors.white)
+            userInput = (cursorX + 1 < self.width - 1) and userInput..event[2] or userInput
+            cursorX = math.min(self.width-1, cursorX + 1)
+        elseif event[1] == "key" then
+            if event[2] == keys.backspace then
+                cursorX = math.max(1, cursorX - 1)
+                searchBar:write(cursorX,cursorY," ",colors.white)
+                userInput = userInput:sub(1,-2)
+            elseif event[2] == keys.enter then
+                inputLoop = false
+                print(textutils.serialize(search:filter(storage.list())))
+            end
+        elseif event[1] == "mouse_click" then
+            if not searchBar:contains(event[3],event[4]) then
+                inputLoop = false
+            end
+            os.queueEvent(table.unpack(event))
+        end
+
+        -- Display updates
+        search:generateKeys(userInput)
+        listing.list = search:filter(storage.list())
+        listing:populate()
+        screen:render()
+        term.setCursorPos(cursorX + self.x - 1, cursorY + self.y - 1)
+    end
+    term.setCursorBlink(false)
+end
+
 local helpText = {
-    'Warehouse GUI help                     ',
-    '=======================================',
-    'To put items into the warehouse:       ',
-    '1. Load the turtle with items          ',
-    '2. Click the "Put" button              ',
-    '                                       ',
-    'To get items from the warehouse:       ',
-    '1. Click the grey box next to the item ',
-    '   you want                            ',
-    '2. Type in how much you want           ',
-    '3. Click the "Get" button              ',
-    '                                       ',
-    'Press enter to continue                ',
+    {
+        'Warehouse GUI help (page 1/2)          ',
+        '=======================================',
+        'To put items into the warehouse:       ',
+        '1. Load the turtle with items          ',
+        '2. Click the "Put" button              ',
+        '                                       ',
+        'To get items from the warehouse:       ',
+        '1. Click the grey box next to the item ',
+        '   you want                            ',
+        '2. Type in how much you want           ',
+        '3. Click the "Get" button              ',
+        '                                       ',
+        'Arrows: Navigation    Enter: Close Help',
+    },
+    {
+        'Warehouse GUI help (page 2/2)          ',
+        '=======================================',
+        'With the search bar, you can type a    ',
+        'list of "keys" you want to display,    ',
+        'separated by spaces.                   ',
+        '                                       ',
+        'Special characters:                    ',
+        '@ - Looks for a mod name.              ',
+        '& - Item names have to match all keys  ',
+        '    attached by &s.                    ',
+        '# - In form "key#num", "key" must have ',
+        '    at least "num" items available.    ',
+        'Arrows: Navigation    Enter: Close Help',
+    }
 }
 function helpButton:onClick()
-    helpDisplay:erase()
-    helpDisplay:fill(colors.gray)
-    for line=1,#helpText do
-        helpDisplay:write(1, line, helpText[line], colors.black)
+    local displayHelp = true
+    local page = 1
+    local event = {}
+    while displayHelp do
+        helpDisplay:erase()
+        helpDisplay:fill(colors.black)
+        for line=1,#helpText[page] do
+            helpDisplay:write(1, line, helpText[page][line], (line == #helpText[page]) and (colors.yellow) or (colors.white))
+        end
+        screen:render()
+        term.setCursorPos(1,screen.height+1)
+
+        event = { os.pullEvent() }
+        if event[1] == "key" then
+            if event[2] == keys.enter then
+                displayHelp = false
+            elseif event[2] == keys.right then
+                page = math.min(page + 1, #helpText)
+            elseif event[2] == keys.left then
+                page = math.max(page - 1, 1)
+            end
+        end
     end
-    screen:render()
-    term.setCursorPos(1,screen.height+1)
-    read()
     helpDisplay:fill(colors.black)
     helpDisplay:fill(colors.white,true)
     helpDisplay:erase()
@@ -266,8 +373,11 @@ while mainLoop do
         end
     elseif event[1] == "mouse_scroll" then
         listing:onScroll(event[2])
-    end
-    if event[1] and event[1] ~= "key_up" and event[1] ~= "mouse_up" then
+        searchBar:erase()
+        searchBar:write(1, 1, "Click here to search...", colors.gray)
+    elseif event[1] == "char" then
+        searchBar:onClick(nil, nil, event[2])
+    elseif event[1] and event[1] ~= "key_up" and event[1] ~= "mouse_up" then
         searchBar:erase()
         searchBar:write(1, 1, "Click here to search...", colors.gray)
     end
