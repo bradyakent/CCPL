8c39ee91a0f0786172cfc2bf8c3cc6190303b161
Brady <bradyakent@gmail.com>
2020-12-11 18:44:30 -0600

    Push changes

--- apis/mining.lua
+++ apis/mining.lua
@@ -35,6 +35,7 @@ local function alreadyQueued(dig, direction, inBounds)
 end
 
 local function matchesFilter(filter, table)
+    if type(filter) ~= type(table) then return false end
     if type(filter) ~= "table" and filter ~= table then return false end
     for key, filterField in pairs(filter) do
         local value2 = table[key]
@@ -212,48 +213,52 @@ local function chaos(filter, amount, lookAhead, handlers)
     local minedTotal = 0
     local fillInHandler = handlers.fillIn
     handlers.fillIn = function(placeFunction) -- Overload the fillIn handler so chaosMined keeps track of vein-mined blocks as well
-        chaosMined[textutils.serialize(tex.getPosition())] = true
+        local currPos = tex.getPosition()
+        if placeFunction == tex.place then
+            local currDir = tex.getDirection()
+            currPos.x = currPos.x + currDir.x
+            currPos.z = currPos.z + currDir.z
+            chaosMined[textutils.serialize(currPos)] = true
+        elseif placeFunction == tex.placeUp then
+            currPos.y = currPos.y + 1
+            chaosMined[textutils.serialize(currPos)] = true
+        elseif placeFunction == tex.placeDown then
+            currPos.y = currPos.y - 1
+            chaosMined[textutils.serialize(currPos)] = true
+        end
         if fillInHandler then fillInHandler(placeFunction) end
     end
     for _=1,amount do
         local currPos = tex.getPosition()
         local currDir = tex.getDirection()
-        local currLookAhead = 2
-        local maxPreference = 0
-        local numOfBlocks = (2/3)*(4*currLookAhead + 3*currLookAhead*currLookAhead + 2*currLookAhead*currLookAhead*currLookAhead) + 1
         local forward, left, right, up, down
-        while currLookAhead <= lookAhead and maxPreference < (2/currLookAhead)*numOfBlocks - currLookAhead do
-            numOfBlocks = (2/3)*(4*currLookAhead + 3*currLookAhead*currLookAhead + 2*currLookAhead*currLookAhead*currLookAhead) + 1
-            forward = {
-                x=currPos.x+currDir.x, y=currPos.y, z=currPos.z+currDir.z,
-                command= function() tex.forward(1, true) end
-            }
-            forward.preference = amtOfNewBlocks(forward, chaosMined, currLookAhead, handlers)
-            left = {
-                x=currPos.x-currDir.z, y=currPos.y, z=currPos.z+currDir.x,
-                command=function() tex.left() tex.forward(1, true) end
-            }
-            left.preference = amtOfNewBlocks(left, chaosMined, currLookAhead, handlers)
-            right = {
-                x=currPos.x+currDir.z, y=currPos.y, z=currPos.z-currDir.x,
-                command=function() tex.right() tex.forward(1, true) end
-            }
-            right.preference = amtOfNewBlocks(right, chaosMined, currLookAhead, handlers)
-            up = {
-                x=currPos.x, y=currPos.y+1, z=currPos.z,
-                command=function() tex.up(1, true) end
-            }
-            up.preference = amtOfNewBlocks(up, chaosMined, currLookAhead, handlers)
-            down = {
-                x=currPos.x, y=currPos.y-1, z=currPos.z,
-                command=function() tex.down(1, true) end
-            }
-            down.preference = amtOfNewBlocks(down, chaosMined, currLookAhead, handlers)
-            maxPreference = math.max(forward.preference, left.preference, right.preference, up.preference, down.preference)
-            currLookAhead = currLookAhead + 1
-        end
+        forward = {
+            x=currPos.x+currDir.x, y=currPos.y, z=currPos.z+currDir.z,
+            command= function() tex.forward(1, true) end
+        }
+        forward.preference = amtOfNewBlocks(forward, chaosMined, lookAhead, handlers)
+        left = {
+            x=currPos.x-currDir.z, y=currPos.y, z=currPos.z+currDir.x,
+            command=function() tex.left() tex.forward(1, true) end
+        }
+        left.preference = amtOfNewBlocks(left, chaosMined, lookAhead, handlers)
+        right = {
+            x=currPos.x+currDir.z, y=currPos.y, z=currPos.z-currDir.x,
+            command=function() tex.right() tex.forward(1, true) end
+        }
+        right.preference = amtOfNewBlocks(right, chaosMined, lookAhead, handlers)
+        up = {
+            x=currPos.x, y=currPos.y+1, z=currPos.z,
+            command=function() tex.up(1, true) end
+        }
+        up.preference = amtOfNewBlocks(up, chaosMined, lookAhead, handlers)
+        down = {
+            x=currPos.x, y=currPos.y-1, z=currPos.z,
+            command=function() tex.down(1, true) end
+        }
+        down.preference = amtOfNewBlocks(down, chaosMined, lookAhead, handlers)
         local sorted = { forward, left, right, up, down }
-        io.open("output.txt","a"):write(tostring((currLookAhead-1).." "..forward.preference.." "..left.preference.." "..right.preference.." "..up.preference.." "..down.preference).."\n"):close()
+        io.open("output.txt","a"):write(tostring(forward.preference.." "..left.preference.." "..right.preference.." "..up.preference.." "..down.preference).."\n"):close()
 
         table.sort(sorted, function(pos1, pos2)
             return pos1.preference > pos2.preference
@@ -269,6 +274,40 @@ local function chaos(filter, amount, lookAhead, handlers)
         chaosMined[textutils.serialize(currPos)] = true
     end
     if handlers.done then handlers.done() end
+end
+
+local function collectVein2(filter, handlers)
+    if isFull() then
+        consolidate()
+        if isFull() and handlers.full then handlers.full() end
+    end
+    local listOfStuff = {}
+    local startingPos = tex.getPosition()
+    local startingDir = tex.getDirection()
+    local _, inspectResults = tex.inspect()
+    local forward = {
+        x=startingPos.x+startingDir.x,
+        y=startingPos.y,
+        z=startingPos.z+startingDir.z,
+        ore=matchesFilter(filter, inspectResults)
+    }
+    _, inspectResults = tex.inspectUp()
+    local up = {
+        x=startingPos.x,
+        y=startingPos.y+1,
+        z=startingPos.z,
+        ore=matchesFilter(filter, inspectResults)
+    }
+    local down = { x=startingPos.x, y=startingPos.y-1, z=startingPos.z }
+    local left = { x=startingPos.x-startingDir.z, y=startingPos.y, z=startingPos.z+startingDir.x }
+    local right = { x=startingPos.x+startingDir.z, y=startingPos.y, z=startingPos.z-startingDir.x }
+    listOfStuff[#listOfStuff+1] = 
+    local collectedVein = false
+    while not collectedVein do
+        for _, block in pairs(startingPosition) do
+            
+        end
+    end
 end
 
 local function layerGetOre(filter, height, currentLayer)
