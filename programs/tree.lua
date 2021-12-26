9acfb7c2b848ac9f6c17bab0191f93477855f4c2
Brady <bradyakent@gmail.com>
2021-12-26 01:09:25 -0600

    Create tree.lua

---
+++ programs/tree.lua
@@ -0,0 +1,67 @@
+local tex = require("/ccpl")("tex")
+
+local args = { ... }
+local length = args[1]
+local width = args[2]
+
+local currentHeight = 0
+
+local function tree(high)
+    tex.forward(1, true)
+    if(not high) then
+        local blockBelow = tex.inspectDown()
+        if(blockBelow.tags["#minecraft:saplings"]) then
+            tex.digDown()
+            while tex.detectUp() do
+                tex.up(1, true)
+                currentHeight = currentHeight + 1
+            end
+        end
+    else
+        while currentHeight > 1 do
+            tex.down(1, true)
+            currentHeight = currentHeight - 1
+        end
+        local blockBelow = tex.inspectDown()
+        if(blockBelow.tags["#minecraft:saplings"]) then
+            tex.digDown()
+        end
+    end
+end
+
+tex.up()
+
+local heightToggle = false
+for x = 1, width do
+    for z = 1, length-1 do
+        tree(heightToggle)
+        if heightToggle then
+            heightToggle = false
+        else
+            heightToggle = true
+        end
+    end
+    if(x % 2 == 1) then
+        tex.turnRight()
+        tree(heightToggle)
+        if heightToggle then
+            heightToggle = false
+        else
+            heightToggle = true
+        end
+        tex.turnRight()
+    else
+        tex.turnLeft()
+        tree(heightToggle)
+        if heightToggle then
+            heightToggle = false
+        else
+            heightToggle = true
+        end
+        tex.turnLeft()
+    end
+end
+
+while currentHeight > 0 do
+    tex.down(1, true)
+end
